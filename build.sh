c7269592a3b41896ca77dc214cbc7b21c2214ea1
Makerosophy <francesco.turlon@gmail.com>
2025-01-06 18:37:00 +0100

    Add executable build script for Vercel

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Configura le directory temporanee per Rust
+CARGO_HOME=/tmp/.cargo
+RUSTUP_HOME=/tmp/.rustup
+PATH=$CARGO_HOME/bin:$PATH
+
+# Installa Rust
+echo "Installing Rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Installa Trunk
+echo "Installing Trunk..."
+cargo install trunk
+
+# Costruisci il progetto
+echo "Building the project..."
+trunk build --release --dist frontend/dist
